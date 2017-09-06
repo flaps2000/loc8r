fc0ec4f8e89d165163dd35f44c9683b97f7bdf26
Martyn Wilson <flaps_2000@yahoo.com>
2017-09-06 20:36:33 +0000

    Chapter 5 < 5.4.2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # loc8r
+
+tutorial from book
+
+interesting mean stack
+  noSQL
+  
+mongodb features
+  2dsphere index, geoJSON
+  
+Changes from tutorial code
+  mostly due to versioning issues
--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('./app_server/models/db');
 
 var index = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+//var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -23,24 +24,36 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 module.exports = app;
